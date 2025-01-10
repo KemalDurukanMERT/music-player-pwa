3896186103a0049aa58cdc17f311e877b30d0d74
Kemal Durukan MERT <103702848+KemalDurukanMERT@users.noreply.github.com>
2025-01-11 00:40:27 +0100

    Geolocation added

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "music-player-cache-v1";
+const urlsToCache = ["/", "/index.html", "/static/js/bundle.js"];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [location, setLocation] = useState("");
 
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          fetch(
+            `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+          )
+            .then((response) => response.json())
+            .then((data) => setLocation(data.city));
+        },
+        () => setLocation("Unavailable")
+      );
+    }
+  }, []);
+
+  return <div>Listening in {location || "Unknown Location"}</div>;
+}
 export default App;
